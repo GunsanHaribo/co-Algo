06faf4b5bd038717b6b46b64417a3d6dd0d4d21a
황지환 <hth99131@gmail.com>
2025-04-09 12:00:04 +0900

    Move Zeroes 와 Minimum Common Value

--- src/main/java/algorithm/P2_Arrays__TwoPointer/p6_Bonus_problems/Two_pointers/p283_Move_Zeroes/p283_Move_Zeroes.java
+++ src/main/java/algorithm/P2_Arrays__TwoPointer/p6_Bonus_problems/Two_pointers/p283_Move_Zeroes/p283_Move_Zeroes.java
@@ -1,4 +1,24 @@
 package algorithm.P2_Arrays__TwoPointer.p6_Bonus_problems.Two_pointers.p283_Move_Zeroes;
 
+import java.util.Arrays;
+
 public class p283_Move_Zeroes {
+    public static void main(String[] args) {
+        int[] nums = {0};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int start = 0;
+        int end = 0;
+        while (end < nums.length) {
+            if (nums[end] != 0) {
+                nums[start] = nums[end];
+                nums[end] = 0;
+                start++;
+            }
+            end++;
+        }
+    }
 }
