4c1edb826a0939546cb22f998f1d0201c1ed528e
황지환 <hth99131@gmail.com>
2025-04-30 12:27:42 +0900

    Longest SubString 풀이

--- src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p3_Longest_Substring_Without_Repeating_Characters/p3_Longest_Substring_Without_Repeating_Characters 풀이.md
+++ src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p3_Longest_Substring_Without_Repeating_Characters/p3_Longest_Substring_Without_Repeating_Characters 풀이.md
@@ -1,7 +1,49 @@
 # 문제
-
+~~~
+문자열 s가 주어졌을 때, 중복된 문자가 없는 가장 긴 부분 문자열의 길이를 구하세요.
+~~~
 ## 문제 분석
+1. 미지 : 부분 문자열의 길이(투포인터 사용)
+2. 자료 : 문자열 s
+3. 조건 : 
+- 중복된 문자가 없어야 된다. -> Set
+- 가장 긴 부분 문자열 : max 저장
 
 ## 문제 풀이
+1. Example
+~~~text
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+~~~
+
+1. 진행과정 
+2. b 
+3. cb
+4. abc
+5. cab
+6. bca 
+   1. right++(a):다음요소가 현재 집합에 이미 있다면 
+   2. set에서 삭제하고 left++(이걸 안겹칠때까지) 
+   3. max 사이즈미교 
+7. abc -> left, right 두고 
+
+
+2. Example
+~~~text
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+~~~
+
+3. Example
+~~~text
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+~~~
+
 
 ## 링크
+https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1621602606
--- src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p3_Longest_Substring_Without_Repeating_Characters/p3_Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p3_Longest_Substring_Without_Repeating_Characters/p3_Longest_Substring_Without_Repeating_Characters.java
@@ -1,4 +1,29 @@
 package algorithm.P3_Hashing.p4_More_hashing_examples.p3_Longest_Substring_Without_Repeating_Characters;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class p3_Longest_Substring_Without_Repeating_Characters {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int maxSize = 0;
+        Set<Character> tempSavedChar = new HashSet<>();
+        char[] charArray = s.toCharArray();
+        for (int right = 0; right < charArray.length; right++) {
+            while (tempSavedChar.contains(charArray[right])) {
+                tempSavedChar.remove(charArray[left]);
+                left++;
+            }
+
+            tempSavedChar.add(charArray[right]);
+            maxSize = Math.max(tempSavedChar.size(), maxSize);
+        }
+
+        return maxSize;
+    }
 }
