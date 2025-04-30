c7b7b8e8165be417af510cee98aebf7cf4322a09
황지환 <hth99131@gmail.com>
2025-04-30 10:37:05 +0900

    Jewel_and_Stone 풀이

--- src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p771_Jewels_and_Stones/p771_Jewels_and_Stones.java
+++ src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p771_Jewels_and_Stones/p771_Jewels_and_Stones.java
@@ -1,4 +1,32 @@
 package algorithm.P3_Hashing.p4_More_hashing_examples.p771_Jewels_and_Stones;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class p771_Jewels_and_Stones {
+    public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAAbbbb";
+
+        System.out.println(numJewelsInStones(jewels, stones));
+    }
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        char[] totalStones = stones.toCharArray();
+        Map<Character, Integer> stoneCount = new HashMap<>();
+        for (char stone : totalStones) {
+            stoneCount.put(stone, stoneCount.getOrDefault(stone, 0) + 1);
+        }
+
+        Set<Character> jewelSet = jewels.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toSet());
+        return stoneCount.entrySet()
+                .stream()
+                .filter(stone -> jewelSet.contains(stone.getKey()))
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+    }
 }
--- src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p771_Jewels_and_Stones/p771_Jewels_and_Stones풀이.md
+++ src/main/java/algorithm/P3_Hashing/p4_More_hashing_examples/p771_Jewels_and_Stones/p771_Jewels_and_Stones풀이.md
@@ -1,7 +1,41 @@
 # 문제
+~~~
+당신은 보석(jewels)을 나타내는 문자열 jewels와, 당신이 가지고 있는 돌(stones)을 나타내는 문자열 stones를 가지고 있습니다.
+stones의 각 문자는 당신이 가진 돌의 한 종류를 나타냅니다.
+이제 당신이 가진 돌들 중 몇 개가 보석인지 알고 싶습니다.
 
+문자들은 대소문자를 구분하므로, "a"는 "A"와 다른 종류의 돌로 간주됩니다
+~~~
 ## 문제 분석
+1. 미지 : 가진 돌 중 몇개가 보석인지, stone의 보석의 갯수 구하기  
+2. 자료 : 문자열 jewels와 문자열 stones(내가 가진돌)
+3. 조건 : 
+- 문자는 대소문자 구분
+- 문자열 
 
 ## 문제 풀이
 
+1. Example
+~~~text
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+~~~
+1. stones을 통해 몇개 가지고 있는지 저장
+2. jewels을 통해 개수 합계 
+
+2. Example 2:
+~~~text
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+~~~
+
+~~~
+sum()은 IntStream, LongStream, DoubleStream에서만 사용할 수 있습니다.
+mapToInt()
+map()은 사용 불가
+.chars() 좋네요
+~~~
+
 ## 링크
+
+https://leetcode.com/problems/jewels-and-stones/submissions/1621548480
