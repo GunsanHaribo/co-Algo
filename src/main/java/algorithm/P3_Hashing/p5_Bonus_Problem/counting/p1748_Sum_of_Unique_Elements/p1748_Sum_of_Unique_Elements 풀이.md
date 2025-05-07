44b1e3f5700d28b243c43ee2ed005c85b513993d
황지환 <hth99131@gmail.com>
2025-05-07 11:01:35 +0900

    Sum of Unique Elements 풀이

---
+++ src/main/java/algorithm/P3_Hashing/p5_Bonus_Problem/counting/p1748_Sum_of_Unique_Elements/p1748_Sum_of_Unique_Elements 풀이.md
@@ -0,0 +1,27 @@
+# 문제
+~~~text
+정수 배열 nums가 주어졌습니다.
+배열에서 유일한 요소란 배열에 정확히 한 번만 등장하는 요소를 의미합니다.
+
+nums의 모든 유일한 요소들의 합을 반환하세요.
+~~~
+
+## 문제 분석
+1. 미지 : 유일한 요소들의 합
+2. 자료 : 정수 배열 nums
+3. 조건 : 
+-  배열에 정확히 한번만 등장하는 요소 = 유일한 요소 
+
+## 문제 풀이
+
+### 1. Example
+~~~text
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+~~~
+1. counting 하고 "1"만 필터링
+
+
+## 링크
+https://leetcode.com/problems/sum-of-unique-elements/submissions/1627443740
--- src/main/java/algorithm/P3_Hashing/p5_Bonus_Problem/counting/p1748_Sum_of_Unique_Elements/p1748_Sum_of_Unique_Elements.java
+++ src/main/java/algorithm/P3_Hashing/p5_Bonus_Problem/counting/p1748_Sum_of_Unique_Elements/p1748_Sum_of_Unique_Elements.java
@@ -1,4 +1,27 @@
 package algorithm.P3_Hashing.p5_Bonus_Problem.counting.p1748_Sum_of_Unique_Elements;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class p1748_Sum_of_Unique_Elements {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+
+        System.out.println(sumOfUnique(nums));
+    }
+
+    public static int sumOfUnique(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        return count.entrySet()
+                .stream()
+                .filter(integerIntegerEntry -> integerIntegerEntry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
 }
