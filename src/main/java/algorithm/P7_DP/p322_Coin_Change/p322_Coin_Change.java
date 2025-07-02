c87e499b24a52a6349f3d8e53b1175f2f8a00ded
황지환 <hth99131@gmail.com>
2025-07-02 20:00:15 +0900

    p322_Coin_Change 풀이

--- src/main/java/algorithm/P7_DP/p322_Coin_Change/p322_Coin_Change.java
+++ src/main/java/algorithm/P7_DP/p322_Coin_Change/p322_Coin_Change.java
@@ -1,9 +1,49 @@
 package algorithm.P7_DP.p322_Coin_Change;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class p322_Coin_Change {
 
+  public static void main(String[] args) {
+    int[] coins = new int[]{1, 2, 5};
+    int amount = 11;
+//    int[] coins = new int[]{2};
+//    int amount = -1;
+
+    p322_Coin_Change solver = new p322_Coin_Change();
+    int answer = solver.coinChange(coins, amount);
+    System.out.println(answer);
+  }
+
+  Map<Integer, Integer> memoMin = new HashMap<>();
+
   public int coinChange(int[] coins, int amount) {
-    return 0;
+    if (amount < 0) {
+      return -1;
+    }
+    if (amount == 0) {
+      return 0;
+    }
+    if (memoMin.containsKey(amount)) {
+      return memoMin.get(amount);
+    }
+
+    int min = Integer.MAX_VALUE;
+    for (int coin : coins) {
+      int subResult = coinChange(coins, amount - coin);
+      // 최소 돌려돌려
+      if (subResult >= 0 && subResult < min) {
+        min = subResult + 1;
+      }
+    }
+
+    if (min == Integer.MAX_VALUE) {
+      min = -1;
+    }
+
+    memoMin.put(amount, min);
+    return memoMin.get(amount);
   }
 
 }
