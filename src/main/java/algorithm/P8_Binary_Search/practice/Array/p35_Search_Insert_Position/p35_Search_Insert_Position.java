c82f68277d6ee12fb28524bf54217572036bbe86
황지환 <hth99131@gmail.com>
2025-07-16 12:08:08 +0900

    p35_Search_Insert_Position 풀이

--- src/main/java/algorithm/P8_Binary_Search/practice/Array/p35_Search_Insert_Position/p35_Search_Insert_Position.java
+++
@@ -1,5 +0,0 @@
-package algorithm.P8_Binary_Search.practice.Array.p35_Search_Insert_Position;
-
-public class p35_Search_Insert_Position {
-
-}
---
+++ src/main/java/algorithm/P8_Binary_Search/practice/array/p35_Search_Insert_Position/p35_Search_Insert_Position.java
@@ -0,0 +1,41 @@
+package algorithm.P8_Binary_Search.practice.array.p35_Search_Insert_Position;
+
+public class p35_Search_Insert_Position {
+
+  public static void main(String[] args) {
+    p35_Search_Insert_Position solver = new p35_Search_Insert_Position();
+
+//    int[] nums = {1, 3, 5, 6};
+//    int target = 5;
+//
+    int[] nums = {1, 3, 5, 6};
+    int target = 2;
+
+    int answerIndex = solver.searchInsert(nums, target);
+    System.out.println(answerIndex);
+  }
+
+  public int searchInsert(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      if (nums[mid] > target) {
+        right = mid - 1;
+        continue;
+      }
+
+      if (nums[mid] < target) {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  }
+
+}
