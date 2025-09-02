5587af0efaa63de35cee417081a2359941c8abc9
황지환 <hth99131@gmail.com>
2025-09-02 09:29:12 +0900

    p100_Same_Tree 보강

--- src/main/java/algorithm/P5_BinaryTree/P2_DFS/example/p100_Same_Tree/retry/p100_Same_Tree.java
+++ src/main/java/algorithm/P5_BinaryTree/P2_DFS/example/p100_Same_Tree/retry/p100_Same_Tree.java
@@ -1,15 +1,32 @@
 package algorithm.P5_BinaryTree.P2_DFS.example.p100_Same_Tree.retry;
 
+import algorithm.P5_BinaryTree.P1_Tree_Maker.TreeHelper;
 import algorithm.P5_BinaryTree.P1_Tree_Maker.TreeNode;
 
 public class p100_Same_Tree {
 
   public static void main(String[] args) {
+//    TreeNode p = TreeHelper.buildTree(new Integer[]{1, 2});
+//    TreeNode q = TreeHelper.buildTree(new Integer[]{1, null, 2});
 
+    TreeNode p = TreeHelper.buildTree(new Integer[]{1, 2, 3});
+    TreeNode q = TreeHelper.buildTree(new Integer[]{1, 2, 3});
+    p100_Same_Tree solver = new p100_Same_Tree();
+    System.out.println(solver.isSameTree(p, q));
   }
 
   public boolean isSameTree(TreeNode p, TreeNode q) {
-    return false;
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null) {
+      return false;
+    }
+
+    boolean left = isSameTree(p.left, q.left);
+    boolean right = isSameTree(p.right, q.right);
+
+    return p.val == q.val && left && right;
   }
 
 }
