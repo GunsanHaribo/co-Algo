2217233b2d8c244a2ecd9dfa0e949a8838ec854c
황지환 <hth99131@gmail.com>
2025-09-03 10:05:50 +0900

    p111_Minimum_Depth_of_Binary_Tree 재풀이

---
+++ src/main/java/algorithm/P5_BinaryTree/P2_DFS/pratice/p1026_Maximum_Difference_Between_Node_and_Ancestor/retry/p1026_Maximum_Difference_Between_Node_and_Ancestor.java
@@ -0,0 +1,15 @@
+package algorithm.P5_BinaryTree.P2_DFS.pratice.p1026_Maximum_Difference_Between_Node_and_Ancestor.retry;
+
+import algorithm.P5_BinaryTree.P1_Tree_Maker.TreeNode;
+
+public class p1026_Maximum_Difference_Between_Node_and_Ancestor {
+
+  public static void main(String[] args) {
+
+  }
+
+  public int diameterOfBinaryTree(TreeNode root) {
+    return 0;
+  }
+
+}
---
+++ src/main/java/algorithm/P5_BinaryTree/P2_DFS/pratice/p543_Diameter_of_Binary_Tree/retry/p543_Diameter_of_Binary_Tree.java
@@ -0,0 +1,15 @@
+package algorithm.P5_BinaryTree.P2_DFS.pratice.p543_Diameter_of_Binary_Tree.retry;
+
+import algorithm.P5_BinaryTree.P1_Tree_Maker.TreeNode;
+
+public class p543_Diameter_of_Binary_Tree {
+
+  public static void main(String[] args) {
+
+  }
+
+  public int maxAncestorDiff(TreeNode root) {
+    return 0;
+  }
+
+}
