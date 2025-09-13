c4e4708e684080e4207a57fd649c07d34d29237f
황지환 <hth99131@gmail.com>
2025-09-14 01:19:37 +0900

    Re_p270_Closest_Binary_Search_Tree_Value 오답

---
+++ src/main/java/algorithm/P5_BinaryTree/P4_BinarySearchTrees/pratice/p270_Closest_Binary_Search_Tree_Value/Re_p270_Closest_Binary_Search_Tree_Value.java
@@ -0,0 +1,58 @@
+package algorithm.P5_BinaryTree.P4_BinarySearchTrees.pratice.p270_Closest_Binary_Search_Tree_Value;
+
+import algorithm.P5_BinaryTree.P1_Tree_Maker.TreeHelper;
+import algorithm.P5_BinaryTree.P1_Tree_Maker.TreeNode;
+
+public class Re_p270_Closest_Binary_Search_Tree_Value {
+
+  public static void main(String[] args) {
+    Re_p270_Closest_Binary_Search_Tree_Value solver = new Re_p270_Closest_Binary_Search_Tree_Value();
+//    TreeNode root = TreeHelper.buildTree(new Integer[]{4, 2, 5, 1, 3});
+//    double target = 3.714286;
+
+    TreeNode root = TreeHelper.buildTree(new Integer[]{1});
+    double target = 4.428571;
+
+    System.out.println(solver.findNearestNode(root, target));
+  }
+
+
+  private int nearestNumber = Integer.MAX_VALUE;
+  private double nearestGap = Double.MAX_VALUE;
+
+  public int findNearestNode(TreeNode root, double target) {
+    // 차가 작은 방향으로 이동합니다. "왼 < 오 = -1"
+    calculateNearest(root, target);
+
+    return nearestNumber;
+  }
+
+  public Integer calculateNearest(TreeNode root, double target) {
+    if (root == null) {
+      return null;
+    }
+    if (Double.compare(calculateAbsGap(root, target), nearestGap) < 0) {
+      nearestGap = calculateAbsGap(root, target);
+      nearestNumber = root.val;
+    }
+
+    double leftGap = calculateAbsGap(root.left, target);
+    double rightGap = calculateAbsGap(root.right, target);
+    int compareLeftRight = Double.compare(leftGap, rightGap);
+    if (compareLeftRight <= 0) {
+      return calculateNearest(root.left, target);
+    }
+
+    return calculateNearest(root.right, target);
+  }
+
+  public double calculateAbsGap(TreeNode node, double target) {
+    if (node == null) {
+      return Double.MAX_VALUE;
+    }
+
+    double gap = (double) node.val - target;
+    return Math.abs(gap);
+  }
+
+}
