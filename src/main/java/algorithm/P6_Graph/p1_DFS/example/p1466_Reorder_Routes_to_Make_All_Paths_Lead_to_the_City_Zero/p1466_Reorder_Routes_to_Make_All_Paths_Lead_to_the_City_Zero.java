25b7eb141d198ef1272afb5b825a55262b58a7df
황지환 <hth99131@gmail.com>
2025-09-30 21:36:43 +0900

    p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero는 내일 푸세요~

--- src/main/java/algorithm/P6_Graph/p1_DFS/example/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.java
+++ src/main/java/algorithm/P6_Graph/p1_DFS/example/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.java
@@ -2,4 +2,24 @@ package algorithm.P6_Graph.p1_DFS.example.p1466_Reorder_Routes_to_Make_All_Paths
 
 public class p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero {
 
+  public static void main(String[] args) {
+    p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero solver = new p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero();
+    int n = 6;
+    int[][] connections = {
+        {0, 1},
+        {1, 3},
+        {2, 3},
+        {4, 0},
+        {4, 5}
+    };
+
+    System.out.println(solver.minReorder(n, connections));
+  }
+
+  public int minReorder(int n, int[][] connections) {
+
+
+    return 0;
+  }
+
 }
