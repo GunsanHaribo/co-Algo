d12198b9291de51177f126e9de8c6d6f00b02581
황지환 <hth99131@gmail.com>
2025-10-13 00:35:28 +0900

    p841_Keys_and_Rooms 일단 보류

--- src/main/java/algorithm/P6_Graph/p1_DFS/a_important/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.java
+++ src/main/java/algorithm/P6_Graph/p1_DFS/a_important/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.java
@@ -1,4 +1,8 @@
-package algorithm.P6_Graph.p1_DFS.example.p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero;
+package algorithm.P6_Graph.p1_DFS.a_important.p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero {
 
@@ -16,10 +20,21 @@ public class p1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero {
     System.out.println(solver.minReorder(n, connections));
   }
 
+  private Map<Integer, HashSet<Integer>> anyWayGraphs = new HashMap<>();
+  private Map<Integer, HashSet<Integer>> wayGraphs = new HashMap<>();
+
+
   public int minReorder(int n, int[][] connections) {
 
 
+
     return 0;
+  }
+
+  // 이전 노드 랑 같이 해서 넣어주던가
+  public void findZero() {
+
+    return;
   }
 
 }
