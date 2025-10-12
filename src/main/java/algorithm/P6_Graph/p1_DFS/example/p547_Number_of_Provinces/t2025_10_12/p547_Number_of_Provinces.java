4feb459627a1a44493ba5ef027ed542f5e18fa62
황지환 <hth99131@gmail.com>
2025-10-12 21:30:16 +0900

    p547_Number_of_Provinces 수도 코드 정리

--- src/main/java/algorithm/P6_Graph/p1_DFS/example/p547_Number_of_Provinces/t2025_10_12/p547_Number_of_Provinces.java
+++ src/main/java/algorithm/P6_Graph/p1_DFS/example/p547_Number_of_Provinces/t2025_10_12/p547_Number_of_Provinces.java
@@ -10,8 +10,36 @@ public class p547_Number_of_Provinces {
     System.out.println(solver.findCircleNum(isConnected));
   }
 
+  private boolean[] seen;
+
   public int findCircleNum(int[][] isConnected) {
-    return 1;
+    int count = 0;
+
+    int n = isConnected.length;
+    seen = new boolean[n];
+
+    for (int i = 0; i < n; i++) {
+      if (!seen[i]) {
+        seen[i] = true;
+        count++;
+        dfs(i, isConnected, n);
+      }
+    }
+
+    return count;
+  }
+
+  // 1하고 연결이 되어 있는 애들 쭉 들어가야됨, 자기자신 제외
+  public void dfs(int nodeNumber, int[][] isConnected, int n) {
+    seen[nodeNumber] = true;
+
+    // 다음으로, isConnected에 1로 되어있는 숫자들만
+    int[] isConnectedRow = isConnected[nodeNumber];
+    for (int i = 0; i < n; i++) {
+      if (!seen[i] && isConnectedRow[i] == 1) {
+        dfs(i, isConnected, n);
+      }
+    }
   }
 
 }
