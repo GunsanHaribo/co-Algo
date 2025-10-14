8132d8898217cd197b81bded8cf2a2f5e0f42ac1
황지환 <hth99131@gmail.com>
2025-10-15 03:25:39 +0900

    p863_All_Nodes_Distance_K_in_Binary_Tree 풀이

--- src/main/java/algorithm/P6_Graph/p2_BFS/example/p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination/t2025_10_14/p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination.java
+++ src/main/java/algorithm/P6_Graph/p2_BFS/example/p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination/t2025_10_14/p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination.java
@@ -2,4 +2,13 @@ package algorithm.P6_Graph.p2_BFS.example.p1293_Shortest_Path_in_a_Grid_with_Obs
 
 public class p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination {
 
+  public static void main(String[] args) {
+
+  }
+
+  public int[][] updateMatrix(int[][] mat) {
+
+    return new int[2][2];
+  }
+
 }
--- src/main/java/algorithm/P6_Graph/p2_BFS/example/p542_01_Matrix/t2025_10_14/p542_01_Matrix.java
+++ src/main/java/algorithm/P6_Graph/p2_BFS/example/p542_01_Matrix/t2025_10_14/p542_01_Matrix.java
@@ -2,4 +2,16 @@ package algorithm.P6_Graph.p2_BFS.example.p542_01_Matrix.t2025_10_14;
 
 public class p542_01_Matrix {
 
+  public static void main(String[] args) {
+    p542_01_Matrix solver = new p542_01_Matrix();
+    int[][] mat = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+
+    System.out.println(solver.updateMatrix(mat));
+  }
+
+  public int[][] updateMatrix(int[][] mat) {
+
+    return new int[2][2];
+  }
+
 }
