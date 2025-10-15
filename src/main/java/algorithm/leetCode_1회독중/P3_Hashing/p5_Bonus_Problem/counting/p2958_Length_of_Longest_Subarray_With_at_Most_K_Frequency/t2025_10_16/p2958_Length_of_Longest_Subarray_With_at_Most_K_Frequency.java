ed2f19d98d243840b5504349604f726fdd22778d
황지환 <hth99131@gmail.com>
2025-10-16 07:41:38 +0900

    p300_Longest_Increasing_Subsequence

---
+++ src/main/java/algorithm/leetCode_1회독중/P3_Hashing/p5_Bonus_Problem/counting/p2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency/t2025_10_16/p2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.java
@@ -0,0 +1,19 @@
+package algorithm.leetCode_1회독중.P3_Hashing.p5_Bonus_Problem.counting.p2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.t2025_10_16;
+
+public class p2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency {
+
+  public static void main(String[] args) {
+    p2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency solver = new p2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency();
+    int[] nums = {1, 2, 3, 1, 2, 3, 1, 2};
+    int k = 2;
+
+    System.out.println(solver.maxSubarrayLength(nums, k));
+  }
+
+  public int maxSubarrayLength(int[] nums, int k) {
+
+
+    return 0;
+  }
+
+}
---
+++ src/main/java/algorithm/leetCode_1회독중/P3_Hashing/p5_Bonus_Problem/general/p791_Custom_Sort_String/t2025_10_15/p791_Custom_Sort_String.java
@@ -0,0 +1,18 @@
+package algorithm.leetCode_1회독중.P3_Hashing.p5_Bonus_Problem.general.p791_Custom_Sort_String.t2025_10_15;
+
+public class p791_Custom_Sort_String {
+
+  public static void main(String[] args) {
+    p791_Custom_Sort_String solver = new p791_Custom_Sort_String();
+    String order = "cba";
+    String s = "abcd";
+
+    System.out.println(solver.customSortString(order, s));
+  }
+
+  public String customSortString(String order, String s) {
+
+    return "";
+  }
+
+}
