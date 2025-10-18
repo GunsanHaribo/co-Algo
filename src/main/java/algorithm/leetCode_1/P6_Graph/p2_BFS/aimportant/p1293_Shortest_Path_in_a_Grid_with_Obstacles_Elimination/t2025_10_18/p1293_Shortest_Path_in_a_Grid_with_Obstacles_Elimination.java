a977ccc12401d9be4d2a9728e6485f45467daff6
황지환 <hth99131@gmail.com>
2025-10-19 02:47:59 +0900

    p1129_Shortest_Path_with_Alternating_Colors 다시풀기

---
+++ src/main/java/algorithm/leetCode_1/P6_Graph/p2_BFS/aimportant/p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination/t2025_10_18/p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination.java
@@ -0,0 +1,25 @@
+package algorithm.leetCode_1.P6_Graph.p2_BFS.aimportant.p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination.t2025_10_18;
+
+public class p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination {
+
+  public static void main(String[] args) {
+    p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination solver = new p1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination();
+    int[][] grid = {
+        {0, 0, 0},
+        {1, 1, 0},
+        {0, 0, 0},
+        {0, 1, 1},
+        {0, 0, 0}
+    };
+    int k = 1;
+
+    System.out.println(solver.shortestPath(grid, k));
+  }
+
+  public int shortestPath(int[][] grid, int k) {
+
+
+    return 0;
+  }
+
+}
