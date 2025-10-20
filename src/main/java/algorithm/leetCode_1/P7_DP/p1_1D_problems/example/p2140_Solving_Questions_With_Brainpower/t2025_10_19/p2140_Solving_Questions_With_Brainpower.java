56dfe491334b988dfe52b7c31e5db74fad1fa3b3
황지환 <hth99131@gmail.com>
2025-10-20 20:56:44 +0900

    p2140_Solving_Questions_With_Brainpower 발림

---
+++ src/main/java/algorithm/leetCode_1/P7_DP/p1_1D_problems/example/p2140_Solving_Questions_With_Brainpower/t2025_10_19/p2140_Solving_Questions_With_Brainpower.java
@@ -0,0 +1,23 @@
+package algorithm.leetCode_1.P7_DP.p1_1D_problems.example.p2140_Solving_Questions_With_Brainpower.t2025_10_19;
+
+public class p2140_Solving_Questions_With_Brainpower {
+
+  public static void main(String[] args) {
+    p2140_Solving_Questions_With_Brainpower solver = new p2140_Solving_Questions_With_Brainpower();
+    int[][] questions = {
+        {3, 2},
+        {4, 3},
+        {4, 4},
+        {2, 5}
+    };
+
+    System.out.println(solver.mostPoints(questions));
+  }
+
+  public long mostPoints(int[][] questions) {
+
+
+    return 0;
+  }
+
+}
