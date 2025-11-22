beaa0a4a1c46b4012227b90e64fd9e238f0eb617
황지환 <hth99131@gmail.com>
2025-11-22 09:17:11 +0900

    백업

--- src/main/java/algorithm/leetCode_1/P8_Binary_Search/p1_on_arrays/example/p2300_Successful_Pairs_of_Spells_and_Potions/p2300_Successful_Pairs_of_Spells_and_Potions.java
+++ src/main/java/algorithm/leetCode_1/P8_Binary_Search/p1_on_arrays/example/p2300_Successful_Pairs_of_Spells_and_Potions/p2300_Successful_Pairs_of_Spells_and_Potions.java
@@ -1,5 +1,7 @@
 package algorithm.leetCode_1.P8_Binary_Search.p1_on_arrays.example.p2300_Successful_Pairs_of_Spells_and_Potions;
 
+import java.util.Arrays;
+
 public class p2300_Successful_Pairs_of_Spells_and_Potions {
 
   public static void main(String[] args) {
@@ -8,15 +10,40 @@ public class p2300_Successful_Pairs_of_Spells_and_Potions {
     int[] potions = {1, 2, 3, 4, 5};
     int success = 7; //[4,0,3]
 
-//    int[] spells = {3,1,2};
-//    int[] potions = {8,5,8};
+//    int[] spells = {3, 1, 2};
+//    int[] potions = {8, 5, 8};
 //    int success = 16; // 2,0,2
 
-    System.out.println(solver.successfulPairs(spells, potions, success));
+    System.out.println(Arrays.toString(solver.successfulPairs(spells, potions, success)));
   }
 
   public int[] successfulPairs(int[] spells, int[] potions, long success) {
-    return new int[2];
+    int[][] matrix = new int[spells.length][potions.length];
+    Arrays.sort(potions);
+    for (int y = 0; y < spells.length; y++) {
+      for (int x = 0; x < potions.length; x++) {
+        matrix[y][x] = spells[y] * potions[x];
+      }
+    }
+
+    int[] answer = new int[spells.length];
+    for (int i = 0; i < matrix.length; i++) {
+      int left = 0;
+      int right = matrix[i].length;
+      int savedMid = 0;
+      while (left < right) {
+        int mid = left + (right - left) / 2;
+        savedMid = mid;
+        if (matrix[i][mid] < success) {
+          left = mid + 1;
+        } else {
+          right = mid;
+        }
+      }
+      answer[i] = Math.max(matrix[i].length - savedMid - 1, answer[i]);
+    }
+
+    return answer;
   }
 
 }
